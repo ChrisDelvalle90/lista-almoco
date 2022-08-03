7bc3b094b71af43bd1ee59cf4aba9d9fbbcb2c13
ChrisDelvalle90 <christian_mskt@hotmail.com>
2022-08-03 16:51:57 -0400

    Atualização do projeto com inserção do catalogo de pratos

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-let pratos = 
-[
-    {
-        addDescricao : document.getElementById("addDescricao"),
-        valorPrato : document.getElementById("valorPrato"),
-        urlPrato : document.getElementById("urlPrato")
-    }
-]
 
-function adicionaPrato()
-{
-    let addDescricao = document.getElementById("addDescricao").value
-    let valorPrato = document.getElementById("valorPrato").value
-    if(addDescricao && valorPrato == "")
+let pratos = []
+
+function adicionaPrato(){
+    let novoPratos = {}
+    novoPratos.urlPrato = document.getElementById("urlPrato").value
+    novoPratos.addDescricao = document.getElementById("addDescricao").value
+    novoPratos.valorPrato = document.getElementById("valorPrato").value
+
+    // document.getElementById("urlPrato").value = ""
+    // document.getElementById("addDescricao").value = ""
+    // document.getElementById("valorPrato").value = ""
+
+    if(novoPratos.urlPrato == "" || novoPratos.addDescricao == "" || novoPratos.valorPrato == "")
     {
         alert("Preencha todos os campos!")
     }
     else
     {
-        pratos.push(addDescricao)
-        pratos.push(valorPrato)
-        document.getElementById("addDescricao", "valorPrato").value = ""
-        //imprimePrato()
-        console.log(pratos)
+        pratos.push(novoPratos)
+        imprimePrato()
     }
 }
 
+
 function imprimePrato()
 {
-    let sectionPratos = document.getElementById("pratos")
-    let cachePratos = ""
+    let sectionPratos = document.getElementById("catalogo")
     sectionPratos.innerHTML = ""
     for(let i = 0; i < pratos.length; i++)
     {
-        cachePratos = pratos[i]
         sectionPratos.innerHTML += `
         <div class="prato">
-            <p>${cachePratos}</p>
+            <img src="${pratos[i].urlPrato}" alt=""/>
+            <p>${pratos[i].addDescricao}</p>
+            <p>${pratos[i].valorPrato}</p>
         </div>
         `
     }
+}
+
+let pratosBkp
+function filtrar(){
+    let filtra = document.getElementById("campoBusca").value
+    pratosBkp = pratos
+    pratos = pratos.filter((elemento) => {
+        return elemento.addDescricao.toLowerCase().includes(filtra.toLowerCase())
+    })
+    document.getElementById("catalogo").innerHTML = ""
+    imprimePrato()
+}
+
+function limpar()
+{
+    pratos = pratosBkp
+    document.getElementById("catalogo").innerHTML = ""
+    imprimePrato()
 }
